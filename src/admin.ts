c1fc3dc80808259866d59b02d5060ceef61d2c01
Neha FSD <48911578+technaoo@users.noreply.github.com>
2023-03-22 09:35:08 +0530

    Delete src directory

--- src/admin.ts
+++
@@ -1,7 +0,0 @@
-export interface Admin {
-    admin_id: number;
-    full_name: String;
-    email: String;
-    password: String;
-  }
-  
--- src/app/product.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-    product_id: number;
-    product_name: String;
-    msrp: String;
-    url: String;
-  }
-  
